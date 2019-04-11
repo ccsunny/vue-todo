e519eb9f553ac88044f11e0448b2175b816318ad
ccsunny <2953345605@qq.com>
2019-04-11 13:18:46 +0800

    简易版todo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div id="cover"></div>
+    <Header />
+    <TodoList />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import TodoList from './components/TodoList'
+import Footer from './components/Footer'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    TodoList,
+    Footer
+  }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  #cover{
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #999;
+    opacity: .9;
+    z-index: -1;
+  }
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <span>Written by {{author}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      author: 'Sunny'
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+.footer{
+  margin-top: 40px;
+  text-align: center;
+  color: #bfbfbf;
+  font-size: 10px;
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+}
+</style>
